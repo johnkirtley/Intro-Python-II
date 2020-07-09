2c358383099cb7b6e3a76d70b863d179f93bc0c5
johnkirtley <kirtleyj16@gmail.com>
2020-07-09 12:37:44 -0700

    :zap: Player Can View Items In Room

--- src/adv.py
+++ src/adv.py
@@ -6,21 +6,21 @@ from player import Player
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons", ['foyer', None, None, None]),
+                     "North of you, the cave mount beckons", ['foyer', None, None, None], ['shovel']),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
 passages run north and east.""", ['overlook', 'outside', 'narrow', None]),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm.""", [None, 'foyer', None, None]),
+the distance, but there is no way across the chasm.""", [None, 'foyer', None, None], ['binoculars']),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
 to north. The smell of gold permeates the air.""", ['treasure', None, None, 'foyer']),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south.""", [None, 'narrow', None, None])
+earlier adventurers. The only exit is to the south.""", [None, 'narrow', None, None], ['treasure chest'])
 }
 
 # Sets player name and initial room location
@@ -34,16 +34,22 @@ player1 = Player(player_name, room['outside'])
 def inputParser():
 
     direction = input(
-        'Where would you like to go next? (n for North, e for East, s for South, w for West, or q to Quit)')
+        '\nWhere would you like to go next? (n for North, e for East, s for South, w for West, or q to Quit)')
 
     return direction
 
 
 while True:
 
-    print(f"{player1.name}'s current location is {player1.current_room.name}")
+    print(f"\n{player1.name}'s current location is {player1.current_room.name}")
 
-    print(f"{player1.current_room.description}")
+    print(f"{player1.current_room.description}\n")
+
+    if len(player1.current_room.items) > 0:
+        print('Items in this room:')
+        player1.current_room.print_items()
+    else:
+        print('No items in this room')
 
     direction = inputParser()
 
@@ -58,4 +64,4 @@ while True:
         if player1.current_room.available_rooms[room_index] != None:
             player1.current_room = room[player1.current_room.available_rooms[room_index]]
         else:
-            print('*****Cannot proceed in that direction. Try again.*****')
+            print('\n*****Cannot proceed in that direction. Try again.*****')
