45c7a9042152a438b7dec7535dbae363522cf985
johnkirtley <kirtleyj16@gmail.com>
2020-07-08 17:00:56 -0700

    :zap: Completed Player Class

--- src/player.py
+++ src/player.py
@@ -1,2 +1,17 @@
-# Write a class to hold player information, e.g. what room they are in
-# currently.
+class Player:
+    def __init__(self, name, current_room):
+        self.name = name
+        self.current_room = current_room
+
+# takes a specified direction and returns an index position
+# this index position is used for the available_rooms attr on the Room class
+
+    def move(self, move):
+        if move == 'n':
+            return 0
+        elif move == 's':
+            return 1
+        elif move == 'e':
+            return 2
+        elif move == 'w':
+            return 3
