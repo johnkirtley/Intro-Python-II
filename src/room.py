10c8949d3e6036febe9d059fa71bd9009a446c13
johnkirtley <kirtleyj16@gmail.com>
2020-07-09 12:37:16 -0700

    :zap: Items Can Be Stored In Rooms

--- src/room.py
+++ src/room.py
@@ -1,8 +1,13 @@
 class Room:
-    def __init__(self, name, description, available_rooms):
+    def __init__(self, name, description, available_rooms, items=[]):
         self.name = name
         self.description = description
+        self.items = items
 
         # array of rooms available to current_room
 
         self.available_rooms = available_rooms
+
+    def print_items(self):
+        for i in self.items:
+            print(i)
