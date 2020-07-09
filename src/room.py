02162ca6c79e9e3149026f4ff6b83a0084af521a
johnkirtley <kirtleyj16@gmail.com>
2020-07-08 17:01:25 -0700

    :zap: Completed Room Class

--- src/room.py
+++ src/room.py
@@ -1,28 +1,8 @@
-# Implement a class to hold room information. This should have name and
-# description attributes.
-
-# Rooms to include are Outside, Foyer, Overlook, Narrow, Treasure
-
-
 class Room:
-    def __init__(self, name, description):
+    def __init__(self, name, description, available_rooms):
         self.name = name
         self.description = description
 
-    def outside(self, n_to='foyer'):
-        self.n_to = n_to
+        # array of rooms available to current_room
 
-    def foyer(self, n_to='overlook', s_to='outside', e_to='narrow'):
-        self.n_to = n_to
-        self.s_to = s_to
-        self.e_to = e_to
-
-    def overlook(self, s_to='foyer'):
-        self.s_to = s_to
-
-    def narrow(self, n_to='treasure', w_to='foyer'):
-        self.n_to = n_to
-        self.w_to = w_to
-
-    def treasure(self, s_to='narrow'):
-        self.s_to = s_to
+        self.available_rooms = available_rooms
